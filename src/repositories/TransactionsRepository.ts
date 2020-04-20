0fe8dcf02b5355750bd2274833228f797a4e8921
rafausatai <rafaela.usatai@gmail.com>
2020-04-19 21:13:11 -0300

    all tests passed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,13 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +22,56 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions.reduce((total, actual) => {
+      if (actual.type === 'income') {
+        return total + actual.value;
+      }
+      return total;
+    }, 0);
+
+    const totalOutcome = this.transactions.reduce((total, actual) => {
+      if (actual.type === 'outcome') {
+        return total + actual.value;
+      }
+      return total;
+    }, 0);
+
+    const totalBalance = totalIncome - totalOutcome;
+
+    return {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalBalance,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const balance = this.getBalance();
+
+    const transaction = new Transaction({ title, type, value });
+
+    switch (transaction.type) {
+      case 'income':
+        this.transactions.push(transaction);
+        break;
+      case 'outcome':
+        if (balance.total >= value) {
+          this.transactions.push(transaction);
+        } else {
+          throw new Error('No sufficient amount for the transaction.');
+        }
+        break;
+      default:
+        throw new Error(
+          'Please choose between Outcome or Income for the transaction.',
+        );
+    }
+
+    return transaction;
   }
 }
 
